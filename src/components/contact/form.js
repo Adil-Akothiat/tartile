da0363d2d32a85ddf3593d296137bcb86b1de959
Adil-Akothiat <adilrar156@gmail.com>
2023-06-12 00:52:38 +0200

    form validation start

--- src/components/contact/form.js
+++ src/components/contact/form.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = ()=> {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [country, setCountry] = useState("");
+    const [age, setAge] = useState("");
+    const [message, setMessage] = useState("");
+    const [level, setLevel] = useState("");
+
+    const valid = ()=> {
+        let isValid = [];
+        if(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(phone)) {
+            isValid.push({name:"phone", valid: true});
+        }else {
+            isValid.push({name:"phone", valid: false});
+        }
+        if(name.length>=2 && name.length <= 25) {
+            isValid.push({name:"name", valid:true});
+        }else {
+            isValid.push({name:"name", valid:false});
+        }
+        if(country.length>=2 && country.length>=20) {
+            isValid.push({name:"country", valid:true});
+        }else {
+            isValid.push({name:"country", valid:true});
+        }
+        if(+age>=4 && +age<=99) {
+            isValid.push({name:"age", valid:true});
+        }else {
+            isValid.push({name:"age", valid:true});
+        }
+        if(level!=="" || level!=="المستوى") {
+            isValid.push({name:"level", valid:true});
+        }else {
+            isValid.push({name:"level", valid:false});
+        }
+        return isValid;
+    }
+    const sendEmail = (e)=> {
+        e.preventDefault();
+        console.log()
+    }
     return (
-        <form>
+        <form onSubmit={sendEmail}>
             <input 
                 type="text" 
                 name="name"
                 id="name"
                 placeholder="الاسم"
+                value={name}
+                onChange={({target})=> setName(target.value)}
                 required
             />
             <input 
@@ -15,31 +58,43 @@ const Form = ()=> {
                 name="mail"
                 id="mail"
                 placeholder="البريد الالكتروني"
+                value={email}
+                onChange={({target})=> setEmail(target.value)}
                 required
             />
             <input 
                 type="number" 
-                name="name"
-                id="name"
+                name="phone"
+                id="phone"
                 placeholder="رقم الهاتف"
+                value={phone}
+                onChange={({target})=> setPhone(target.value)}
                 required
             />
             <input 
                 type="text" 
-                name="name"
-                id="name"
+                name="country"
+                id="country"
                 placeholder="البلد"
+                value={country}
+                onChange={({target})=> setCountry(target.value)}
                 required
             />
             <input 
                 type="number" 
-                name="name"
-                id="name"
+                name="age"
+                id="age"
                 placeholder="السن"
                 maxLength="2"
+                value={age}
+                onChange={({target})=> setAge(target.value)}
                 required
             />
-            <select>
+            <select
+                defaultValue="المستوى"
+                onChange={(e) => setLevel(e.target.value)}
+            >
+                <option value="">المستوى</option>
                 <option value="مبتدئ">مبتدئ</option>
                 <option value="متوسط">متوسط</option>
                 <option value="ماهر">ماهر</option>
@@ -47,6 +102,8 @@ const Form = ()=> {
             <textarea 
                 placeholder="النص"
                 maxLength="250"
+                value={message}
+                onChange={({target})=> setMessage(target.value)}
             />
             <button type="submit">إرسال</button>
         </form>

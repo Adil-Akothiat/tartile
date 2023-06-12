3df7df372c574a476d35b297c5eb5ddb9f093c80
Adil-Akothiat <adilrar156@gmail.com>
2023-06-12 16:03:08 +0200

    add form validation

--- src/components/contact/form.js
+++ src/components/contact/form.js
@@ -1,111 +1,159 @@
 import React, { useState } from "react";
+import validation from "./validation";
 
-const Form = ()=> {
+const Form = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [country, setCountry] = useState("");
     const [age, setAge] = useState("");
     const [message, setMessage] = useState("");
-    // const [level, setLevel] = useState("");
+    const [level, setLevel] = useState("");
+    const [alert, setAlert] = useState({});
 
-    // const valid = ()=> {
-    //     let isValid = [];
-    //     if(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(phone)) {
-    //         isValid.push({name:"phone", valid: true});
-    //     }else {
-    //         isValid.push({name:"phone", valid: false});
-    //     }
-    //     if(name.length>=2 && name.length <= 25) {
-    //         isValid.push({name:"name", valid:true});
-    //     }else {
-    //         isValid.push({name:"name", valid:false});
-    //     }
-    //     if(country.length>=2 && country.length>=20) {
-    //         isValid.push({name:"country", valid:true});
-    //     }else {
-    //         isValid.push({name:"country", valid:true});
-    //     }
-    //     if(+age>=4 && +age<=99) {
-    //         isValid.push({name:"age", valid:true});
-    //     }else {
-    //         isValid.push({name:"age", valid:true});
-    //     }
-    //     if(level!=="" || level!=="المستوى") {
-    //         isValid.push({name:"level", valid:true});
-    //     }else {
-    //         isValid.push({name:"level", valid:false});
-    //     }
-    //     return isValid;
-    // }
-    const sendEmail = (e)=> {
+
+    const sendEmail = (e) => {
         e.preventDefault();
-        // console.log()
+        const {
+            isValidName,
+            isValidPhone,
+            isValidCountry,
+            isValidAge,
+            isValidLevel,
+            isValidEmail,
+            isValidMessage
+        } = validation;
+        if(isValidName(name)&&isValidPhone(phone)&&isValidCountry(country)&&isValidAge(age)&&isValidLevel(level)&&isValidEmail(email)&&isValidMessage) {
+            setName("");setPhone("");setCountry("");setAge("");setEmail("");setMessage("");
+            document.querySelector("select").children[0].selected="select";
+            setAlert({open: true, valid: true, message:"تم التسجيل بنجاح!"});
+        }else {
+            setAlert({open: true, valid: false, message:"المرجو ملئ الإستمارة بطريقة صحيحة"});
+        }
     }
     return (
         <form onSubmit={sendEmail}>
-            <input 
-                type="text" 
+            <input
+                type="text"
                 name="name"
                 id="name"
                 placeholder="الاسم"
                 value={name}
-                onChange={({target})=> setName(target.value)}
+                onChange={({ target }) => {
+                    setName(target.value);
+                    if (validation.isValidName(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
                 required
             />
-            <input 
-                type="email" 
+            <input
+                type="email"
                 name="mail"
                 id="mail"
                 placeholder="البريد الالكتروني"
                 value={email}
-                onChange={({target})=> setEmail(target.value)}
+                onChange={({ target }) => {
+                    setEmail(target.value);
+                    if (validation.isValidEmail(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
                 required
             />
-            <input 
-                type="number" 
+            <input
+                type="number"
                 name="phone"
                 id="phone"
                 placeholder="رقم الهاتف"
                 value={phone}
-                onChange={({target})=> setPhone(target.value)}
+                onChange={({ target }) => {
+                    setPhone(target.value);
+                    if (validation.isValidPhone(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
                 required
             />
-            <input 
-                type="text" 
+            <input
+                type="text"
                 name="country"
                 id="country"
                 placeholder="البلد"
                 value={country}
-                onChange={({target})=> setCountry(target.value)}
+                onChange={({ target }) => {
+                    setCountry(target.value);
+                    if (validation.isValidCountry(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
                 required
             />
-            <input 
-                type="number" 
+            <input
+                type="number"
                 name="age"
                 id="age"
                 placeholder="السن"
                 maxLength="2"
                 value={age}
-                onChange={({target})=> setAge(target.value)}
+                onChange={({ target }) => {
+                    setAge(target.value);
+                    if (validation.isValidAge(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
                 required
             />
             <select
                 defaultValue="المستوى"
-                // onChange={(e) => setLevel(e.target.value)}
+                onChange={({ target }) => {
+                    setLevel(target.value);
+                    if (validation.isValidLevel(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
             >
                 <option value="">المستوى</option>
                 <option value="مبتدئ">مبتدئ</option>
                 <option value="متوسط">متوسط</option>
                 <option value="ماهر">ماهر</option>
             </select>
-            <textarea 
+            <textarea
+                name="النص"
                 placeholder="النص"
                 maxLength="250"
                 value={message}
-                onChange={({target})=> setMessage(target.value)}
+                onChange={({ target }) => {
+                    setMessage(target.value);
+                    if (validation.isValidMessage(target.value)) {
+                        target.classList.remove("danger");
+                    } else {
+                        target.classList.add("danger");
+                    }
+                }}
             />
             <button type="submit">إرسال</button>
+            {
+                alert.open?(
+                    <div className={alert.valid?"alert-msg alert-success":"alert-msg  alert-danger"}>
+                        <p>
+                            {alert.message}
+                        </p>
+                    </div>
+                ):null
+            }
         </form>
     );
 }
---
+++ src/components/contact/validation.js
@@ -0,0 +1,60 @@
+const isValidName = (name)=> {
+    if (name.length >= 2 && name.length <= 25) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const isValidPhone = (phone)=> {
+    if (/^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/.test(phone)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const isValidCountry = (country)=> {
+    if (country.length >= 2 && country.length <= 20) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const isValidAge = (age)=> {
+    if (+age >= 4 && +age <= 99) {
+        return true;
+    } else {
+        return false;
+    }
+}
+const isValidLevel = (level)=> {
+    if(level==="") {
+        return false;
+    }else {
+        return true;
+    }
+}
+const isValidEmail = (email)=> {
+    if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(email)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+const isValidMessage = (message)=> {
+    if (message.length>=10 && message.length<=250) {
+        return true;
+    }else {
+        return false;
+    }
+}
+const validation = {
+    isValidName,
+    isValidPhone,
+    isValidCountry,
+    isValidAge,
+    isValidLevel,
+    isValidEmail,
+    isValidMessage
+}
+
+export default validation;
